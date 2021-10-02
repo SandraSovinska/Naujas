4e089371f9db98d300223cf1ce5e383ac8c139c9
SandraSovinska <90636952+SandraSovinska@users.noreply.github.com>
2021-10-02 19:00:34 +0300

    6.4

---
+++ 6.4Uzduotis/6.4Uzduotis/Controllers/StudensController.cs
@@ -0,0 +1,68 @@
+﻿using _6._4Uzduotis.Models;
+using _6._4Uzduotis.Models.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _6._4Uzduotis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudensController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (student.Name == "")
+            {
+                return ValidationProblem("Nenurodete vardo!");
+            }
+
+            if (student.Surname == "")
+            {
+                return ValidationProblem("Nenurodete pavardes!");
+            }
+
+            if (student.BirthDay == DateTime.MinValue) 
+            {
+                return ValidationProblem("Nenurodete Gimimo dtaos!");
+            }
+
+            var service = new StudentService();
+            service.CreateStudent(student);
+
+            return Ok();
+        }
+
+        [HttpGet("list")]
+        public IActionResult List()
+        {
+            var service = new StudentService();
+            var students = service.GetStudents();
+            return new OkObjectResult(students);
+        }
+
+        [HttpGet]
+        public IActionResult Get(string documentId)
+        {
+            var service = new StudentService();
+            var student = service.GetStudent(documentId);
+
+            return new OkObjectResult(student);
+        }
+
+    }
+
+
+
+}
+        
+        
+       
+
+
+
+
